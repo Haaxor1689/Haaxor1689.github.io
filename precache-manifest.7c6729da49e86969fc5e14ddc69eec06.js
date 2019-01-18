70688c6148f652991aee74b6df093cdaf7989729
Haaxor1689 <betkomaros@gmail.com>
2019-01-18 15:39:10 +0100

    Updates

---
+++ precache-manifest.7c6729da49e86969fc5e14ddc69eec06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe5d8070cc2d0ecd32cdc3dd9852b2d7",
+    "url": "/static/media/avatar.fe5d8070.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8aa3fd0f8eb8314f22e6",
+    "url": "/static/js/main.8aa3fd0f.chunk.js"
+  },
+  {
+    "revision": "355922f3aa586a178588",
+    "url": "/static/js/1.355922f3.chunk.js"
+  },
+  {
+    "revision": "8aa3fd0f8eb8314f22e6",
+    "url": "/static/css/main.4ed2f334.chunk.css"
+  },
+  {
+    "revision": "612b4bfc71825aa6213c3570ec61cf9d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2d9e8524a84727834e330e3cc08c7a03.js"
+  "/precache-manifest.7c6729da49e86969fc5e14ddc69eec06.js"
 );
 
 workbox.clientsClaim();
