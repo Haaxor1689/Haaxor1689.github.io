77e7d945405a60f3f411b68c99287f7c9f4a96dd
Haaxor1689 <betkomaros@gmail.com>
2019-01-20 00:10:43 +0100

    Added more projects.

--- src/Components/DateRange.tsx
+++ src/Components/DateRange.tsx
@@ -4,7 +4,7 @@ export default class DateRange extends React.Component<{ start: string, end?: st
     private getEnd = (): string => this.props.end || "Present";
     render = (): JSX.Element => (
         <span className="date-range">
-            {this.props.start} - {this.getEnd()}
+            {this.props.start} { this.props.start !== this.getEnd() && `- ${this.getEnd()}` }
         </span>
     )
 }
